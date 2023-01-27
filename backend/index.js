0f3bfb7c089b0451d1bea6195810012e8ef65495
Alex Zerah <alex.zerah@gmail.com>
2023-01-27 09:18:50 +0100

    implement socket

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const http = require('http');
+const {Server} = require("socket.io");
 
+const app = express();
 const port = 3000;
 
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Sockets
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    socket.on("/evenementRoute", (d) => {
+        console.log(d);
+    })
+});
+
+// Routes
 app.get('/', (req, res) => {
     console.log(req.query.year);
     res.json({msg: "Hello world"});
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.get('/school', (req, res) => {
@@ -17,11 +41,7 @@ app.get('/school', (req, res) => {
     res.json({msg: "Hello world"});
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
-})
-
-app.listen(port, () => {
+// Ecoute du server
+httpServer.listen(port, () => {
     console.log(`On écoute sur le port ${port}`);
 });
